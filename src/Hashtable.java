c65dd09b8eb7920bc2eddb4044cd1ea3a41ecf41
Volodymyr_Krasnikov <Volodymyr_Krasnikov@EPUAKYIW0792.kyiv.epam.com>
2014-01-30 18:08:40 +0200

    Rabin-Karp

--- src/Hashtable.java
+++ src/Hashtable.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 
 public class Hashtable<K,V> {
     
@@ -14,7 +16,7 @@ public class Hashtable<K,V> {
     private static final int DEFAULT = 16;
     private static final float LOAD_FACTOR = 0.75f;
     
-    private Entry[] arr = new Entry[DEFAULT];
+    private Entry[] table = new Entry[DEFAULT];
     
     private int size;
     
@@ -23,8 +25,8 @@ public class Hashtable<K,V> {
     }
     
     public V get(K key){
-        int idx = indexOf(key, arr.length);
-        for( Entry e = arr[idx]; e != null; e = e.next ){
+        int idx = indexOf(key, table.length);
+        for( Entry e = table[idx]; e != null; e = e.next ){
             if( e.key.equals(key) ){
                 return (V) e.value;
             }
@@ -42,14 +44,14 @@ public class Hashtable<K,V> {
             resize();
         }
         
-        int idx = indexOf(key, arr.length);
+        int idx = indexOf(key, table.length);
                
-        arr[idx] = new Entry(key, value, arr[idx]);
+        table[idx] = new Entry(key, value, table[idx]);
         size++;
         
         V oldValue = null;
         
-        for( Entry e = arr[idx]; e != null && oldValue == null; e = e.next ){
+        for( Entry e = table[idx]; e != null && oldValue == null; e = e.next ){
             Entry next = e.next;
             if( next != null && next.key.equals(key) ){
                 oldValue = (V) next.value;
@@ -62,10 +64,10 @@ public class Hashtable<K,V> {
     }
     
     public void remove(K key){
-        int idx = indexOf(key, arr.length);
-        Entry e = arr[idx];
+        int idx = indexOf(key, table.length);
+        Entry e = table[idx];
         if( e.key.equals(key) ){
-            arr[idx] = e.next;
+            table[idx] = e.next;
             size--;
         } else {
             for(; e != null; e = e.next ){
@@ -87,23 +89,23 @@ public class Hashtable<K,V> {
     }
     
     private void resize(){
-        Entry[] newArr = new Entry[arr.length * 2];
-        for( Entry e : arr ){
+        Entry[] newTable = new Entry[table.length * 2];
+        for( Entry e : table ){
             if( e != null ){
                 for(; e.next != null; e = e.next){
                     put( (K)e.key, (V)e.value );
                 }
             }
         }
-        arr = newArr;
+        table = newTable;
     }
     
     private float loadFactor(){
-        return size() / arr.length;
+        return size() / table.length;
     }
     
     public void clear(){
-        arr = new Entry[DEFAULT];
+        Arrays.fill(table, null);
         size = 0;
     }
     
@@ -136,7 +138,7 @@ public class Hashtable<K,V> {
         
         System.out.println(map.size());
         
-        for(Entry e : map.arr){
+        for(Entry e : map.table){
             int count = 0;
             for(Entry ee = e; ee != null; ee = ee.next){
                 count++;
--- src/strings/RK.java
+++ src/strings/RK.java
@@ -2,9 +2,82 @@ package strings;
 
 public class RK {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        
+        int B = 'z' - 'a' + 1;
+        int M = 179453731;
+        
+//        baaaaababbaa
+//        abbab
+        
+//        aaaabaaaaabbaaaa
+//        aa
 
-	}
+        char[] str = "aaaabaaaaabbaaaa".toCharArray();
+        char[] p = "aa".toCharArray();
+        
+        int L = p.length;
+        
+        int H = hash(p, 0, L, M, B);
+        int C = hash(str, 0, L, M, B);
+        
+        int offset = 0;
+        
+        do {
+            if( C == H ){
+                System.out.println(offset);
+            }
+            offset += 1;
+            if( offset <= str.length - L ){
+                C = shift(str, C, offset, L, M, B);
+            }
+        } while(offset <= str.length - L);
+
+    }
+    
+    static int shift(char[] str, int H, int i, int m, int M, int B){
+        int tmp = mul(str[i-1], pow(B, m - 1), M);
+        return add( mul( (H - tmp), B, M), str[i + m - 1], M);
+    }
+
+    static int hash(char[] str, int i, int m, int M, int B) {
+        int H = 0;
+        for(int k = 0; k < m; k++){
+            int H1 = mul( str[i + k], pow(B, m - k - 1), M );
+            H = add(H, H1, M);
+        }
+        return H;
+    }
+
+    private static int add(int a, int b, int M) {
+        return (a % M + b % M) % M;
+    }
+
+    private static int mul(int a, int b, int M) {
+        return (a % M * b % M) % M;
+    }
+    
+    static int modpow(int a, int n, int M) {
+        a %= M;
+        int result = 1;
+        while (n > 0) {
+            if ((n & 1) == 1)
+                result = (result * a) % M;
+            a = (a * a) % M;
+            n >>= 1;
+        }
+        return result;
+    }
+    
+    private static int pow(int a, int n){
+        if( n == 0 ) return 1;
+        if( n == 1 ) return a;
+        if( n % 2 > 0){
+            return a * pow(a, n - 1);
+        } else {
+            int t = pow(a, n/2);
+            return t * t;
+        }
+    }
 
 }
