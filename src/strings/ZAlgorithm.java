1ffd5c14dcb8de04c179deacc67b93c9bbd007e0
Volodymyr_Krasnikov <Volodymyr_Krasnikov@EPUAKYIW0792.kyiv.epam.com>
2014-02-06 18:51:19 +0200

    Z algorithm intro

---
+++ src/strings/ZAlgorithm.java
@@ -0,0 +1,12 @@
+package strings;
+
+// Volodymyr_Krasnikov1 <vkrasnikov@gmail.com> 6:50:58 PM 
+
+public class ZAlgorithm {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
