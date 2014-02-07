84436a4f3015717bcb5c71e90b36a31c9fee6912
Volodymyr_Krasnikov <Volodymyr_Krasnikov@EPUAKYIW0792.kyiv.epam.com>
2014-02-07 19:00:55 +0200

    z algorithmen

--- src/graphs/shortestpath/Dijkstra.java
+++ src/graphs/shortestpath/Dijkstra.java
@@ -1,5 +1,37 @@
 package graphs.shortestpath;
 
-public class Dijkstra {
+import graphs.AbstractGraphFactory;
+import graphs.GraphFactory;
+import graphs.GraphPrinter;
+import graphs.Weighted;
+import graphs.GraphFactory.StorageType;
 
+public class Dijkstra {
+    
+    public static int[] distanceToAll(Weighted g, int from){
+        return null;
+    }
+
+    
+    public static void main(String[] args) {
+        
+        GraphFactory EDGE_LIST_GRAPH_FACTORY = AbstractGraphFactory.getFactory(StorageType.EDGE_LIST);
+        Weighted g = EDGE_LIST_GRAPH_FACTORY.weightedGraph(6);
+        
+        g.addEdge(0, 1, 7);
+        g.addEdge(0, 2, 9);
+        g.addEdge(0, 5, 14);
+        
+        g.addEdge(1, 2, 10);
+        g.addEdge(1, 3, 15);
+        
+        g.addEdge(2, 3, 11);
+        g.addEdge(2, 5, 2);
+        
+        g.addEdge(3, 4, 6);
+        g.addEdge(5, 4, 9);
+        
+        GraphPrinter.print(g);
+        
+    }
 }
--- src/strings/ZAlgorithm.java
+++ src/strings/ZAlgorithm.java
@@ -5,8 +5,27 @@ package strings;
 public class ZAlgorithm {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
+        String str = "aabadaabcaaba";
+        String pat = "aab";
+        
+        char[] s = str.toCharArray();
+        char[] p = pat.toCharArray();
+        
+        char[] S = (pat + '$' + str).toCharArray();
+        
+        int[] Z = zAlgorithmen(S);
+        
+        for( int offset = p.length + 1; offset < Z.length; offset++ ){
+            if(Z[offset] == p.length)
+                System.out.println(offset);
+        }
 
+    }
+
+    private static int[] zAlgorithmen(char[] S) {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
