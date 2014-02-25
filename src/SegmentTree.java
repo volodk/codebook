e1690b2239af9c13b46dca9e63cd62823c00ba72
Volodymyr_Krasnikov <Volodymyr_Krasnikov@EPUAKYIW0792.kyiv.epam.com>
2014-02-25 18:51:51 +0200

    segment tree and skiplist

---
+++ src/SegmentTree.java
@@ -0,0 +1,6 @@
+
+// Volodymyr_Krasnikov1 <vkrasnikov@gmail.com> 5:38:23 PM 
+
+public class SegmentTree {
+
+}
---
+++ src/SkipList.java
@@ -0,0 +1,6 @@
+
+// Volodymyr_Krasnikov1 <vkrasnikov@gmail.com> 5:39:15 PM 
+
+public class SkipList {
+
+}
