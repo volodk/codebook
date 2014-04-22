f9fa8c60823988cbd3ce62d9c5604e3a10011383
Volodymyr_Krasnikov <Volodymyr_Krasnikov@EPUAKYIW0792.kyiv.epam.com>
2014-04-22 13:24:54 +0300

    sparse table for max/min range queries

--- src/SparseTable.java
+++ src/SparseTable.java
@@ -1,11 +1,9 @@
 import java.util.Arrays;
 
-
-
 public class SparseTable<T> {
     
     interface Function<T> {
-        T call(T a, T b);  // any associative function like min, max, mul, avg, gcd, lcm ... 
+        T call(T a, T b);  // associative function like min, max,
     }
     
     Object[][] t;
@@ -15,24 +13,26 @@ public class SparseTable<T> {
         
         int N = values.length;
         
-        t = new Object[N][log2(N)];
+        t = new Object[N][ log2(N) + 1];
         
         for(int i = 0; i < N; i++){ t[i][0] = values[i]; }
         
-        for( int j = 1; j < log2(N); j++ ){
+        for( int j = 1; j <= log2(N); j++ ){
             for(int i = 0; i < N; i++){
-                t[i][j] = function.call( (T) t[i][j-1],  (T) t[ Math.min(i + (1 << j-1), N-1)][j-1]);
+                t[i][j] = function.call( (T) t[i][j-1],  (T) t[ Math.min( i + (1 << j-1), N-1)][j-1]);
             }
         }
     }
 
     @SuppressWarnings("unchecked")
     public T query(int i, int j){
-        return (T) t[i][ log2(j-i+1) - 1];
+        if( i > j)
+            throw new IllegalArgumentException();
+        return (T) t[i][ log2(j-i+1) ];
     }
     
-    public void print(int N){
-        for(int i = 0; i < N; i++){
+    public void print(){
+        for(int i = 0; i < t.length; i++){
             System.out.println(Arrays.toString(t[i]));
         }
     }
@@ -40,19 +40,20 @@ public class SparseTable<T> {
     
     public static void main(String[] args) {
         
-        Function<Integer> lambda = new Function<Integer>() {
+        Function<Integer> fun = new Function<Integer>() {
             @Override
             public Integer call(Integer a, Integer b) {
-//                return Math.min(a, b);
-                return Math.max(a, b);
+                return Math.min(a, b);
+//                return Math.max(a, b);
+                
             }
         };
         
-        SparseTable<Integer> st = new SparseTable<Integer>( lambda, 1, 3, 4, 2, 5, 7, 6 );
+        SparseTable<Integer> st = new SparseTable<Integer>( fun, 1,2,3,4,5 );
         
-        st.print(7);
+        st.print();
         
-        System.out.println( st.query(0, 6));
+        System.out.println( st.query(1, 4) );
     }
     
     private static int log2(int n) {
