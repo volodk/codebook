c6d5cf581fee2ce16ee30160ab2724e0ffed14b4
volodymyr krasnikov <vkrasnikov@gmail.com>
2014-04-25 23:58:18 +0300

    int min heap

--- src/heap/MinHeap.java
+++ src/heap/MinHeap.java
@@ -1,98 +1,76 @@
 package heap;
 
-import java.lang.reflect.Array;
+import java.util.NoSuchElementException;
+import java.util.Random;
 
 public class MinHeap {
     
     int N = 16;
-    
-    int head = 0, tail = -1;
+    int head = 1, tail = 0;
     int[] arr;
     
-    @SuppressWarnings("unchecked")
-    public MinHeap() {
-        arr = new int[N]; 
-    }
+    public MinHeap() { arr = new int[N]; }
     
     public void push(int a){
-        
         ensureSize();
-        
         arr[++tail] = a;
-        
         int i = tail;
-        while( arr[i] < arr[i/2] ){
+        while( i > head && arr[ i/2 ] > arr[ i ] ){
             swap(arr, i, i/2);
             i = i / 2;
         }
-        
     }
     
     public int pop(){
-        if( tail < 0 )
-            throw new RuntimeException("Exhausted heap");
-        
+        checkIndex();
         int top = arr[head];
-        
         arr[head] = arr[tail--];
-        
         int i = head;
-        
-        while( isMinHeap(i) ){
-            
-            int j = minChildOf(i);
-            
+        while( 2 * i <= tail && ( arr[i] > arr[2 * i] || arr[i] > arr[2 * i + 1] ) ){
+            int j = 2 * i + ( arr[ 2 * i ] < arr[ 2 * i + 1] ? 0 : 1 ); 
             swap(arr, i, j);
-            
             i = j;
         }
-        
         return top;
     }
-    
-    private int minChildOf(int i) {
-        return 0;
+
+    private void checkIndex() {
+        if ( tail < 1 ) throw new NoSuchElementException();
     }
 
-    private boolean isMinHeap(int i) {
-        boolean left = 2*i+1 <= tail && arr[i] < arr[2*i+1];
-        boolean rigth = 2*i+2 <= tail && arr[i] < arr[2*i+2];
-        return left && rigth;
-    }
-
-    public int size(){
-        return tail + 1;
-    }
+    public int size(){ return tail; }
     
     private void ensureSize(){
-        if( tail == N - 1 ){
-            doubleInSize(); 
+        if( size() == N - 1 ){
+            int[] buff = new int[N * 2];
+            System.arraycopy(arr, 0, buff, 0, N);
+            arr = buff;
+            N = N * 2;
         }
     }
-    
-    private void doubleInSize(){
-        int[] copy = new int[2*N];
-        System.arraycopy(arr, 0, copy, 0, N);
-        arr = copy;
-    }
-    
     
     private void swap(int[] arr, int i, int j){
         int t = arr[i]; arr[i] = arr[j]; arr[j] = t;
     }
     
     public static void main(String[] args) {
-        
         MinHeap h = new MinHeap();
         
-        h.push(3);
-        h.push(1);
-        h.push(2);
-        
-        while( h.size() > 0){
-            System.out.println(h.pop());
-        }
+        Random rnd = new Random();
 
+        for(int i = 0; i < 100000; i++){
+            h.push( rnd.nextInt() );
+        }
+        
+        int p = h.pop();
+        while( h.size() > 0){
+            int c = h.pop();
+            if(c < p){
+                throw new RuntimeException();
+            } else {
+                p = c;
+            }
+        }
     }
 
 }
