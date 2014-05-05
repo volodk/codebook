b391de13e3d6a400b559fcc17e9ba6ad18a60f0d
volodymyr krasnikov <vkrasnikov@gmail.com>
2014-05-05 22:59:14 +0300

    min heap ds

--- src/heap/Heaps.java
+++ src/heap/Heaps.java
@@ -1,17 +1,15 @@
 package heap;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Random;
 
 // Volodymyr_Krasnikov1 <vkrasnikov@gmail.com> 12:35:43 PM 
 
+@SuppressWarnings("unchecked")
 public class Heaps {
     
-    public interface Heap <T extends Comparable<? super T>> {
+    public interface Heap <T> {
         void push(T elem);
         T pop();
         T peek();
@@ -20,25 +18,24 @@ public class Heaps {
     
     private Heaps(){}
     
-    private static class ArrayHeap<T extends Comparable<? super T>> implements Heap<T>{
+    private static class ArrayHeap<T> implements Heap<T>{
         
-        private int N = 16;
+        private int N = 256;
         private int head = 1, tail = 0;
-        private ArrayList<T> arr;
+        private Object[] arr;
         private Comparator<T> comparator;
 
         public ArrayHeap(Comparator<T> comparator) {
             this.comparator = comparator;
-            arr = new ArrayList<>(N);
-            arr.add(null);
+            arr = new Object[N];
         }
 
         @Override
         public void push(T elem) {
             ensureSize();
-            arr.add(elem); tail++;
+            arr[++tail] = elem;
             int i = tail;
-            while( i > head && gt( arr.get(i/2), arr.get(i) )  ){
+            while( i > head && gt( arr[i/2], arr[i] )  ){
                 swap(arr, i, i/2);
                 i = i / 2;
             }
@@ -47,26 +44,29 @@ public class Heaps {
         @Override
         public T pop() {
             checkIndex();
-            T top = arr.get(head);
-            arr.set( head, arr.get(tail) );
-            arr.set( tail, null);   // prevent memory leaks 
-            tail--;
+            Object top = arr[head];
+            
+            arr[head] = arr[tail];
+            arr[tail] = null;   // prevent memory leaks 
+            tail -= 1;
+            
             int i = head;
-            while( 2 * i <= tail && ( gt(arr.get(i), arr.get( 2*i ) ) || gt( arr.get(i), arr.get( 2*i + 1) ) ) ){
-                int k = 0;
-                if( 2*i + 1 < tail ){
-                    k = lt(arr.get(2*i), arr.get(2*i+1))? 0 : 1;
+            T val;
+            while (2 * i < tail && gt(arr[i], val = min(arr[2 * i], arr[2 * i + 1])) || 2 * i <= tail
+                    && gt(arr[i], val = min(arr[i], arr[2 * i]))) {
+                if( gt( arr[i], val) ){
+                    int j = 2 * i + ( eq( arr[2*i], val) ? 0 : 1 );
+                    swap(arr, i, j);
+                    i = j;
                 }
-                int j = 2 * i + k;
-                swap(arr, i, j);
-                i = j;
-            }
-            return top;
+            } 
+            
+            return (T) top;
         }
 
         @Override
         public T peek() {
-            return (T) arr.get(head);
+            return (T) arr[head];
         }
         
         @Override
@@ -78,28 +78,21 @@ public class Heaps {
         
         private void ensureSize(){
             if( size() == N - 1 ){
-                ArrayList<T> buff = new ArrayList<>(N * 2);
-                buff.addAll(arr);
+                Object buff[] = new Object[2*N];
+                System.arraycopy(arr, 0, buff, 0, N);
                 arr = buff;
                 N = N * 2;
             }
         }
         
-        private void swap(List<?> arr, int i, int j){
-            Collections.swap(arr, i, j);
+        private void swap(Object[] arr, int i, int j){
+            Object t = arr[i]; arr[i] = arr[j]; arr[j] = t;
         }
         
-        private static <T extends Comparable<? super T>> boolean lt(T obj1, T obj2){
-            return obj1.compareTo(obj2) < 0;
-        }
+        private T min(Object obj1, Object obj2){ return comparator.compare( (T)obj1, (T)obj2) < 0 ? (T)obj1 : (T)obj2; }
         
-        private static <T extends Comparable<? super T>> boolean gt(T obj1, T obj2){
-            return obj1.compareTo(obj2) > 0;
-        }
-        
-        private static <T extends Comparable<? super T>> boolean eq(T obj1, T obj2){
-            return obj1.compareTo(obj2) == 0;
-        }
+        private boolean gt(Object obj1, Object obj2){  return comparator.compare((T)obj1, (T)obj2) > 0;  }
+        private boolean eq(Object obj1, Object obj2){  return comparator.compare((T)obj1, (T)obj2) == 0; }
         
     }
     
@@ -122,7 +115,7 @@ public class Heaps {
     }
     
     public static void main(String[] args) {
-        Heap<Integer> heap = Heaps.maxHeap();
+        Heap<Integer> heap = Heaps.minHeap();
         
         Random rnd = new Random();
 
@@ -130,16 +123,9 @@ public class Heaps {
             heap.push( rnd.nextInt(100) );
         }
         
-        int p = heap.pop();
-        System.out.println(p);
-        while( heap.size() > 0){
-            int c = heap.pop();
-            System.out.println(p);
-            if(c > p){
-                throw new RuntimeException(c + ":" +p);
-            } else {
-                p = c;
-            }
+        while(heap.size() > 0){
+            System.out.println(heap.pop());
         }
+        
     }
 }
