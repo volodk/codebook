3d0d503e88e5aabc241dd7a8c957c5cc581ccfa9
volodymyr krasnikov <vkrasnikov@gmail.com>
2014-05-11 23:53:07 +0300

    segment tree (initial ver)

--- src/SegmentTree.java
+++ src/SegmentTree.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 
 // Volodymyr_Krasnikov1 <vkrasnikov@gmail.com> 5:38:23 PM 
 
@@ -7,6 +9,53 @@ public class SegmentTree<T> {
         T call(T a, T b);  // any associative function like min, max, mul, avg, gcd ... 
     }  
     
-    private Function<T> funtion;
+    private final Function<T> function;
+    
+    private int[] segments;
+    
+    public SegmentTree(Function<T> function) {
+        this.function = function;
+    }
+    
+    public void build(T ... values){
+        Objects.requireNonNull(values);
+        int N = values.length;
+        segments = new int[ N + 1];
+        int node = 1;
+        int i = 0, j = N-1;
+        
+        build( node, i, j, values, segments );
+    }
+    
+    private void build(int node, int b, int e, T[] values, int[] segments){
+        
+        if(b == e)
+            segments[node] = b;
+        
+        build( node * 2, b, (b + e)/2, values, segments);
+        build( node * 2 + 1, (b + e)/2 + 1, e, values, segments);
+        
+        // calc function over child nodes
+        
+    }
+    
+    public T query(int i, int j){
+        return null;
+    }
+    
+        
+    public static void main(String[] args) {
+        
+        SegmentTree<Integer> st = new SegmentTree<Integer>(new Function<Integer>() {
+            @Override
+            public Integer call(Integer a, Integer b) {
+                return Math.min(a, b);
+            }
+        });
+        
+        st.build( 3,1,4,5,2,6,3,7,4 );
+        
+        System.out.println( st.query(1,5) );
+    }
     
 }
