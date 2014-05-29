9164f5b1f6d36bef566a89a93fc74d70497d8329
Volodymyr_Krasnikov <Volodymyr_Krasnikov@EPUAKYIW0792.kyiv.epam.com>
2014-05-29 16:31:53 +0300

    gcd & sqrt numeric algorithms

---
+++ src/GCD.java
@@ -0,0 +1,19 @@
+
+// Volodymyr_Krasnikov1 <vkrasnikov@gmail.com> 12:07:34 PM 
+
+public class GCD {
+
+    public static void main(String[] args) {
+        
+        System.out.println( gcd( 14, 21 ) );
+
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+//        return gcd(b, a % b);
+        if( a > b) return gcd(b, a - b); 
+        return gcd(a, b - a);
+    }
+
+}
---
+++ src/SQRT.java
@@ -0,0 +1,28 @@
+
+// Volodymyr_Krasnikov1 <vkrasnikov@gmail.com> 12:11:41 PM 
+
+public class SQRT {
+
+    public static void main(String[] args) {
+        
+//        float y = 0.16f;
+        float y = 16f;
+        
+        System.out.println( sqrt(y) );
+
+    }
+    
+    public static float sqrt( float y ){
+        
+        if( y < 0) throw new IllegalArgumentException("Cannot be negative");
+        
+        double e = 0.000000001f;
+        double x0 = y, x1 = 0.0;
+        while( Math.abs( (x0 - x1) / x0 ) > e){
+            x0 = (x0 + x1)/2;
+            x1 = y / x0;
+        }
+        return (float)x1;
+    }
+
+}
