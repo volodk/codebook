6775326a76f9316f49cf208b203421bc9cb3c272
volodymyr krasnikov <vkrasnikov@gmail.com>
2014-10-23 23:25:38 +0300

    use hash sorting for gather char statistics (for small texts only)

--- src/huffman/Client.java
+++ src/huffman/Client.java
@@ -6,7 +6,7 @@ public class Client {
 
     public static void main(String[] args) {
         
-        String original = "encode me";
+        String original = "creating a huffman tree is simple. sort this list by frequency";
         
         char[] text = original.toCharArray();
         
